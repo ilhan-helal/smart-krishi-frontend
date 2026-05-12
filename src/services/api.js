3bbcf93002496e52142cec0887ac6a7de1969760
ILHAN HELAL <ilhanhelal30@gmail.com>
2026-05-12 15:09:56 +0530

    Connected frontend with deployed backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://smart-krishi-backend-58n9.onrender.com/api",
 });
 
 export default API;
